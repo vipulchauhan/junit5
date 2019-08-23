ac22b9c68458c63efcc33eea5cf457e0163e8074
vipul <08bce016@gmail.com>
2019-08-24 01:02:22 +0530

    written test cases

--- junit5/src/main/java/junit5/MathUtils.java
+++ junit5/src/main/java/junit5/MathUtils.java
@@ -5,4 +5,8 @@ public class MathUtils {
 	public int sum(int a, int b) {
 		return a + b;
 	}
+
+	public Long calculateCircleArea(Integer radius) {
+		return Math.round(Math.PI * radius * radius);
+	}
 }
--- junit5/src/test/java/junit5/MathUtilsTest.java
+++ junit5/src/test/java/junit5/MathUtilsTest.java
@@ -1,12 +1,25 @@
 package junit5;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
 
 	@Test
-	void testSum() {
-		System.out.println("worked");
+	void test() {
+		MathUtils mathUtils = new MathUtils();
+		Integer ip1 = 10, ip2 = 20, expectedOutput = 30;
+		Integer actualOutput = mathUtils.sum(ip1, ip2);
+		assertEquals(expectedOutput, actualOutput, "the sum answer is incorrect");
+	}
+
+	@Test
+	void testCircleArea() {
+		MathUtils mathUtils = new MathUtils();
+		Integer radius = 10;
+		Long actualArea = mathUtils.calculateCircleArea(radius);
+		assertEquals(314L, actualArea, "circle area is incorect");
 	}
 
 }
